f2e2ff099deca1928b1bf8dc3fe3a6f07fee1157
benjaminpfleghaar <info@benjaminpfleghaar.com>
2025-09-22 17:53:37 +0200

    feat: type supabase and fetch data

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,11 @@
 import { cookies } from "next/headers";
+import { Database } from "@/types/supabase";
 import { createServerClient } from "@supabase/ssr";
 
 export async function createClient() {
   const cookieStore = await cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
