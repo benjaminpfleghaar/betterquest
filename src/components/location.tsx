8ab5cb3a9f62ced1f1f6a821fd0d5d260d27bbd8
benjaminpfleghaar <info@benjaminpfleghaar.com>
2025-10-16 17:18:36 +0200

    refactor: add favicon and remove fragments

--- src/components/location.tsx
+++ src/components/location.tsx
@@ -60,42 +60,40 @@ export default async function Location({ slug }: { slug: string }) {
   }*/
 
   return (
-    <>
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-[1fr_3rem]">
-        <div className="relative overflow-hidden rounded-2xl border border-stone-200 bg-white">
-          <MapContainer
-            photo="/forest.jpg"
-            position={[49.177902777777774, 9.285983333333332]}
-          />
-          <div className="p-6 space-y-4">
-            <div className="grid grid-cols-[3rem_1fr] items-center gap-3 pl-1">
-              <div className="flex size-10 items-center justify-center text-white">
-                <div
-                  className={cn(
-                    "absolute size-10 rotate-45 rounded-lg",
-                    background,
-                  )}
-                ></div>
-                {icon}
-              </div>
-              <div>
-                <h1 className={`${jakarta.className} font-bold`}>{title}</h1>
-                <h2 className="text-xs text-stone-600">2 days ago</h2>
-              </div>
-            </div>
-            <p className="text-sm text-stone-600">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua.
-            </p>
-          </div>
-        </div>
-        <SocialLinks
-          lat={49.177902777777774}
-          lon={9.285983333333332}
-          issue={title}
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-[1fr_3rem]">
+      <div className="relative overflow-hidden rounded-2xl border border-stone-200 bg-white">
+        <MapContainer
+          photo="/forest.jpg"
+          position={[49.177902777777774, 9.285983333333332]}
         />
+        <div className="p-6 space-y-4">
+          <div className="grid grid-cols-[3rem_1fr] items-center gap-3 pl-1">
+            <div className="flex size-10 items-center justify-center text-white">
+              <div
+                className={cn(
+                  "absolute size-10 rotate-45 rounded-lg",
+                  background,
+                )}
+              ></div>
+              {icon}
+            </div>
+            <div>
+              <h1 className={`${jakarta.className} font-bold`}>{title}</h1>
+              <h2 className="text-xs text-stone-600">2 days ago</h2>
+            </div>
+          </div>
+          <p className="text-sm text-stone-600">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </p>
+        </div>
       </div>
-    </>
+      <SocialLinks
+        lat={49.177902777777774}
+        lon={9.285983333333332}
+        issue={title}
+      />
+    </div>
   );
 }
