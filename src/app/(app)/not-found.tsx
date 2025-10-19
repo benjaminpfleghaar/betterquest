8dcf055e2f277e6850fc92fb5d23345ab7d33bb9
benjaminpfleghaar <info@benjaminpfleghaar.com>
2025-10-19 17:15:35 +0200

    feat: create not-found page

--- src/app/(app)/not-found.tsx
+++ src/app/(app)/not-found.tsx
@@ -1,11 +1,31 @@
 import Link from "next/link";
+import Logo from "@/components/logo";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="p-4 space-y-4 md:space-y-16">
+      <header>
+        <Link
+          href="/"
+          className="inline-flex rounded-sm outline-offset-2 outline-blue-500 focus-visible:outline-2"
+        >
+          <Logo variant="stone" />
+        </Link>
+      </header>
+      <main className="mx-auto w-full max-w-md md:max-w-lg">
+        <div className="flex aspect-video flex-col items-center justify-center gap-2 rounded-2xl bg-white">
+          <h1 className="text-xl font-bold text-stone-900">404</h1>
+          <p className="mb-2 text-sm text-stone-500">
+            Could not find requested page
+          </p>
+          <Link
+            href="/"
+            className="flex cursor-pointer items-center justify-center rounded-lg bg-stone-900 px-4 py-3 text-sm font-medium text-white outline-offset-2 outline-blue-500 focus-visible:outline-2"
+          >
+            Return Home
+          </Link>
+        </div>
+      </main>
     </div>
   );
 }
